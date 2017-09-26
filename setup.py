13f4876197d56950e9888de0f4de9c89e25cd0fe
Paul Becotte <paul@bam-x.com>
2017-09-26 09:52:52 -0400

    use company email address

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     license='MIT',
     long_description=open('README.md').read(),
     # metadata for upload to PyPI
-    author="Paul Becotte",
-    author_email="pjbecotte@gmail.com",
+    author="The Narrativ Company, Inc.",
+    author_email="admin@narrativ.com",
     description="Enforce contract tests based on yml data files",
     keywords="testing contract tests",
     test_suite='tests/test_contractual.py',
