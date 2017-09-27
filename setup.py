8f533996b2f19bc7b37e42e51bf7270ee66a2f51
Paul Becotte <paul@bam-x.com>
2017-09-27 11:34:02 -0400

    a few pypi updates

--- setup.py
+++ setup.py
@@ -2,10 +2,9 @@ from setuptools import setup
 
 setup(
     name='Contractual',
-    version='0.1',
+    version='0.1.3',
     packages=['contractual'],
     license='MIT',
-    long_description=open('README.md').read(),
     # metadata for upload to PyPI
     author="The Narrativ Company, Inc.",
     author_email="admin@narrativ.com",
